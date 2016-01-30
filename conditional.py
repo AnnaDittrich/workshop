d079b2cd9a5d44245c6049e2e5d0fe22547aef9f
Anna Nelson Dittrich <AnnaND@dhcp-10-132-187-173.uawifi.arizona.edu>
2016-01-30 16:22:06 -0700

    SWC workshop initial commit

---
+++ conditional.py
@@ -0,0 +1,9 @@
+x = 100
+day = "Friday"
+if not (x < 10):
+    print("The day is " + day + 
+          ". What a great day.") # the way this is written allows us to put code on two lines
+elif day == "Friday":
+    print("I don't like Fridays")
+else:
+    print("I don't think this will happen")
---
+++ iteration.py
@@ -0,0 +1,12 @@
+books = ["Tale of Two Cities", "Harry Potter", "Lord of the Rings"]
+nums = range(100)
+
+for book in books:
+    if book == "Harry Potter":
+        print(book)
+    else:
+        print("something else")
+    
+for num in nums:
+    print(num)
+    
